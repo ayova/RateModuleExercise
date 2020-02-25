77f28ac98f641d965223c14f5a4bf204c700987f
Agu <agustin.ovari@gmail.com>
2020-02-25 19:33:38 +0100

    task 2 start

---
+++ RateModules/LandingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LandingViewController.swift
+//  RateModules
+//
+//  Created by ADMIN on 25/02/2020.
+//  Copyright © 2020 adriantineo. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
